7457bd8a406bde190d07edd7d39218b9cc01c77e
Imtiaj Iqbal Mahfuj <imtiajofficials@gmail.com>
2025-12-24 04:07:33 +0600

    Update data.js

--- data.js
+++ data.js
@@ -154,12 +154,12 @@ window.SITE = {
       details:""
     },
     {
-      id:"25000",
-      title:"",
-      tags:[""],
-      image:"",
-      github:"",
-      details:""
+      id:"25027",
+      title:"3D Lunar Topography: Interactive Moon Surface Map",
+      tags:["Geospatial Python", "GeoViz"],
+      image:"https://raw.githubusercontent.com/imtiajiqbalmahfuj/imtiajiqbal-portfolio/refs/heads/main/Projects/25027_3D_Map_of_Moon_suface_using_NASA_data_and_Python/MOON.jpg",
+      github:"https://raw.githubusercontent.com/imtiajiqbalmahfuj/imtiajiqbal-portfolio/refs/heads/main/Projects/25027_3D_Map_of_Moon_suface_using_NASA_data_and_Python/Moon.gif",
+      details:"https://imtiajiqbalmahfuj.github.io/projects/25027_3D_Map_of_Moon_suface_using_NASA_data_and_Python.html"
     },
     {
       id:"25026",
