0c9288652d84f17801dd966ba10c3f18966d9db6
Imtiaj Iqbal Mahfuj <imtiajofficials@gmail.com>
2026-09-08 16:05:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1024,7 +1024,7 @@ function initDefaultParticles() {
   // Capped at max 50 particles to avoid locking up CPU
   // Limits particles to just 12 on mobile to save CPU, while keeping 50 on PC
   const isMobileDevice = window.innerWidth <= 768;
-  let particleCount = isMobileDevice ? 12 : Math.min((canvas.width * canvas.height) / 25000, 50);
+  let particleCount = isMobileDevice ? 0 : Math.min((canvas.width * canvas.height) / 25000, 50);
   for (let i = 0; i < particleCount; i++) {
     particles.push(new Particle());
   }
