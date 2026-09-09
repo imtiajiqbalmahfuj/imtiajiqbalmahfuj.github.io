a955070b10945a2918a500c7bbbb585bebec1e61
Imtiaj Iqbal Mahfuj <imtiajofficials@gmail.com>
2026-09-10 03:18:48 +0600

    Adjust polygon altitude values on hover

--- script.js
+++ script.js
@@ -1122,7 +1122,7 @@ function mountMagicMode() {
                 </div>
               `)
               .onPolygonHover(hoverD => world
-                .polygonAltitude(d => d === hoverD ? 0.12 : 0.06)
+                .polygonAltitude(d => d === hoverD ? 0.08 : 0.03)
                 .polygonCapColor(d => d === hoverD ? 'steelblue' : colorScale(getVal(d)))
               ).polygonsTransitionDuration(300);
 
